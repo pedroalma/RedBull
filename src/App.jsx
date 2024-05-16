30041f76816073449b301eef077fc78c96a29dcb
pedroalma <pedroalma554@gmail.com>
2024-05-16 11:56:51 -0300

    RB20

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SafeAreaView,Text } from "react-native";
+
+export default props=>{
+    return(
+        <SafeAreaView>
+            <Text>RedBull</Text>
+        </SafeAreaView>
+    )
+}
